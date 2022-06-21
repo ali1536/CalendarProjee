ec496010dde6412c3c98d8e6a446e82ae6e735b7
ali1536 <92683569+ali1536@users.noreply.github.com>
2022-06-21 09:42:01 +0430

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Form } from 'react-bootstrap';
-import './style.css';
+import './style.css'
 
 class MyComponent extends Component {
     render() {
         return (
             <div className='text-center '>
-                <Form>
-                    <div className='nam_famil'>
-                        <div className='d-inline'> Name :
-                            <input type='text' placeholder='Enter name ' required />
-                        </div>
-                        <div className='d-inline'> Family :
-                            <input type='text' placeholder='Enter family' required />
-                        </div>
+                <Form> <br /><br /><br />
+                    <div className="input-group mt-5">
+                        <span className="input-group-text bg-gray fw-bold" >First Name</span>
+                        <input type="text" className="form-control mw-25" />
+                        <span className="input-group-text bg-gray fw-bold" >Last Name</span>
+                        <input type="text" className="form-control mw-25" />
                     </div>
                     <div className='calender'>
                         <div className='image'>
-                            <h2> 1 , Wednesday</h2>
+                            <h2> 21 , Tuesday</h2>
                             <h3> June &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2022</h3>
                         </div>
                         <div className='date'>
@@ -37,11 +35,12 @@ class MyComponent extends Component {
                             <div className='number'>4</div>
                             <div className='number'>5</div>
                             <div className='number'>6</div>
+                            <div className='number'>7</div>
                             <div className='number'>8</div>
                             <div className='number'>9</div>
                             <div className='number'>10</div>
                             <div className='number'>11</div>
-                            <div className='number active'>12</div>
+                            <div className='number'>12</div>
                             <div className='number'>13</div>
                             <div className='number'>14</div>
                             <div className='number'>15</div>
@@ -50,7 +49,7 @@ class MyComponent extends Component {
                             <div className='number'>18</div>
                             <div className='number'>19</div>
                             <div className='number'>20</div>
-                            <div className='number'>21</div>
+                            <div className='number active'>21</div>
                             <div className='number'>22</div>
                             <div className='number'>23</div>
                             <div className='number'>24</div>
@@ -63,58 +62,77 @@ class MyComponent extends Component {
                             <div className='number'>31</div>
                         </div>
                     </div>
-                    <div className='hei_wei'>
-                        <div className='d-inline'> Height :
-                            <input required />
+                    <div className="input-group my-3">
+                        <span className="input-group-text bg-gray fw-bold" >Height</span>
+                        <input type="text" className="form-control mw-25" />
+                        <span className="input-group-text bg-gray fw-bold" >Weight</span>
+                        <input type="text" className="form-control mw-25" />
+                    </div>
+                    <div className='px-5 fw-bold'> History Training : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios" />Yes
                         </div>
-                        <div className='d-inline'> Weight :
-                            <input required />
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios" />No
                         </div>
                     </div>
-                    <div className='History_Training'>
-                        <div> History Training :
-                            <input name='test' type='radio' required /> Yes
-                            <input name='test' type='radio' required /> No
-                        </div>
-                    </div>
-                    <div> Number Days of Training sessions per week :
-                        <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
+                    <div >
+                        <select className="form-select" aria-label="Default select example">
+                            <option className='bg-dark fw-bold' >Number Days of Training sessions per week</option>
+                            <option className='bg-dark' >1</option>
+                            <option className='bg-dark' >2</option>
+                            <option className='bg-dark' >3</option>
+                            <option className='bg-dark' >4</option>
+                            <option className='bg-dark' >5</option>
+                            <option className='bg-dark' >6</option>
+                            <option className='bg-dark' >7</option>
                         </select>
                     </div>
-                    <div className='Injury'> Injury :
-                        <input name='test1' type='radio' required /> Yes
-                        <input name='test1' type='radio' required /> No
+                    <div className='px-5 fw-bold'> Injury : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios1" />Yes
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios1" />No
+                        </div>
                     </div>
-                    <div className='Time_Training'>Time Training :
-                        <input placeholder='Hours of training per week' required />
+                    <div className="input-group my-3 fw-bold">
+                        <span className="input-group-text bg-gray fw-bold" >Time Training</span>
+                        <input type="text" className="form-control mw-25" />
                     </div>
-                    <div className='Aerobic'> Aerobic Training :
-                        <input name='test2' type='radio' required /> Yes
-                        <input name='test2' type='radio' required /> No
+                    <div className='px-5 fw-bold'> Aerobic Training : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios2" />Yes
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios2" />No
+                        </div>
                     </div>
-                    <div className='Limite'> Limite Move :
-                        <input name='test3' type='radio' required /> Yes
-                        <input name='test3' type='radio' required /> No
+                    <div className='px-5 fw-bold'> Limite Move  : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios3" />Yes
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios3" />No
+                        </div>
                     </div>
-                    <div className='Muscle'>  Muscle division :
-                        <input name='test4' type='radio' required /> Yes
-                        <input name='test4' type='radio' required /> No
+                    <div className='px-5 fw-bold'> Muscle Division : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios4" />Yes
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="exampleRadios4" />No
+                        </div>
                     </div>
-                    <div className='Supplement'> Sports Supplement :
-                        <input placeholder='Enter your Sports Supplement name' required />
+                    <div className="input-group my-3 fw-bold">
+                        <span className="input-group-text bg-gray fw-bold" >Sports Supplement</span>
+                        <input type="text" className="form-control mw-25" />
                     </div>
-                    <div> Please Send Your Image For Us <br />
+                    <div className='fw-bold'> Please Send Your Image For Us <br />
                         <input type="image" alt='There is no photo' />
                     </div>
-                    <button type='submit'>Next</button>
-                    <button type='reset'>Reset</button>
+                    <button type="button" class="mx-5 btn btn-secondary btn-lg fw-bold w-25">Next</button>
+                    <button type="button" class="mx-5 btn btn-secondary btn-lg fw-bold w-25">Reset</button>
                 </Form>
             </div>
         )
@@ -122,4 +140,3 @@ class MyComponent extends Component {
 }
 
 export default MyComponent;
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyComponent from './App'
+import MyComponent from './App';
 
 
 ReactDOM.render(<React.StrictMode>
